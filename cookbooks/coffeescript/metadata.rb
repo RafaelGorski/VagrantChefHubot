76412be9f8c7a577b33a0583a02a7a495cadcb5e
Rafael Gorski <rafael_gorski@hotmail.com>
2013-08-10 13:05:28 -0300

    Update metadata.rb

--- cookbooks/coffeescript/metadata.rb
+++ cookbooks/coffeescript/metadata.rb
@@ -1,5 +1,5 @@
-maintainer       "AdaptLean"
-maintainer_email "rafael@adaptlean.com.br"
+maintainer       "RafaelGorski.com"
+maintainer_email "rafael@hotmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures Coffee Script via NPM"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
