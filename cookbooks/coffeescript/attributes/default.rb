eb13800b63df90f37eff5335303a79642a5a6aa3
Rafael Gorski <rafael_gorski@hotmail.com>
2013-08-10 13:06:00 -0300

    Update default.rb

--- cookbooks/coffeescript/attributes/default.rb
+++ cookbooks/coffeescript/attributes/default.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: coffeescript 
 # Attributes:: coffeescript 
 #
-# Copyright 2012, AdaptLean.com.br 
+# Copyright 2012, rafaelgorski.com
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
