6c3594ade2abf5daf221df08d79e3b0abb9cc13e
Rafael Gorski <rafael_gorski@hotmail.com>
2013-08-29 21:39:29 -0300

    upgrade to newest version of vagrant

--- cookbooks/coffeescript/metadata.rb
+++ cookbooks/coffeescript/metadata.rb
@@ -1,7 +1,7 @@
-maintainer       "AdaptLean"
-maintainer_email "rafael@adaptlean.com.br"
-license          "Apache 2.0"
+maintainer       "RafaelGorski.com"
+maintainer_email "rafael@rafaelgorski.com"
+license          "Unlicensed"
 description      "Installs/Configures Coffee Script via NPM"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
-recipe           "coffeescript", "Installs Coffee Script from npm"
+recipe           "coffeescript", "Installs Coffee Script using npm"
--- cookbooks/hubot/metadata.rb
+++ cookbooks/hubot/metadata.rb
@@ -1,6 +1,6 @@
 maintainer       "RafaelGorski"
-maintainer_email "rafael_gorski@hotmail.com"
-license          "Apache 2.0"
+maintainer_email "rafael@rafaelgorski.com"
+license          "No License attached"
 description      "Installs/Configures Hubot"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
