0fc9974851ec8fe11ebd29dc8a8018e62e045996
Rafael Gorski <rafael_gorski@hotmail.com>
2013-09-05 20:51:00 -0300

    wip

---
+++ cookbooks/cookbooks-update.bat
@@ -0,0 +1,6 @@
+REM Update the cookbooks needed
+
+git clone https://github.com/librato/nodejs-cookbook.git nodejs;
+git clone https://github.com/opscode-cookbooks/build-essential.git;
+git clone https://github.com/opscode-cookbooks/git.git;
+git clone https://github.com/opscode-cookbooks/apt.git;
---
+++ cookbooks/cookbooks-update.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# Update the cookbooks needed
+#
+git clone https://github.com/librato/nodejs-cookbook.git nodejs;
+git clone https://github.com/opscode-cookbooks/build-essential.git;
+git clone https://github.com/opscode-cookbooks/git.git;
+git clone https://github.com/opscode-cookbooks/apt.git;
