ab64d2420a925e29c1e0fa353d8356246aa6d8ca
Rafael Gorski <rafael_gorski@hotmail.com>
2013-09-11 19:57:41 -0300

    new branch for oracle connector

--- bash_provisioning.sh
+++ bash_provisioning.sh
@@ -44,3 +44,5 @@ git clone https://github.com/kchmck/vim-coffee-script.git .vim/bundle/vim-coffee
 echo "execute pathogen#infect()" >> .vimrc;
 echo "syntax on" >> .vimrc;
 echo "filetype plugin indent on" >> .vimrc;
+
+
